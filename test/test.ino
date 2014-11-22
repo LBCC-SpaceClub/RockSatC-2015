ac31a9747d4ddcff97a0a87e1059f7feb0899fa1
Levi Willmeth <lo9key@gmail.com>
2014-11-21 21:34:02 -0800

    Added SD logging to the sketch, not very robust yet.  Needs more work.

--- test/test.ino
+++ test/test.ino
@@ -1,5 +1,7 @@
+#include <SdFat.h>
+
 #define buffer_max 255  // Max size of each buffer
-#define time_max 60000  // Max runtime
+#define time_max 3600000  // Max runtime (60 min)
 
 #define gateA 40
 #define gateB 41
@@ -8,23 +10,28 @@
 #define pinE 44
 #define pinF 45
 
+#define SDPin 4  // CS pin for SD card socket, pin 4 on ethernet shield
+
 typedef struct gdata {
   /*
   Each reading has a 4 byte timestamp, and 1 byte of data
   Variables used by interrupts should be called volatile
   */
-  volatile unsigned long time;      // Our timestamp, limited to 71! minutes of run time
+  volatile unsigned long time;      // Timestamp, limited to 71! minutes of run time
   volatile byte tubes;              // 1 byte holds all 6, bit-sized measurements
 };
 
 gdata buffer[2][buffer_max+1];  // An array of 2 buffers, with space for 256 readings each
-boolean active_buffer;        // determines which buffer is ready for input
-byte buffer_index = 0;        // Counter to determine when the buffer is full
+boolean active_buffer;        // Determines which buffer is ready for input
+byte buffer_index = 0;        // Counter to hold current place in the buffer
+SdFat sd;
+SdFile dataFile;
 
 void setup(){
   // Debugging only, using serial is very slow.
-  Serial.begin(115200);
-  Serial.println("LBCC RockSat-C code warming up...");
+  Serial.begin(9600); // 115200 or 9600?
+  Serial.println("LBCC RockSat-C warming up...");
+  delay(400);  // Solves Due reset problem
   
   // Prepare input pins and begin interrupts
   // Pin num, function name, event to listen for
@@ -42,15 +49,26 @@ void setup(){
   attachInterrupt(pinE, interrupt_tube_E, FALLING);
   pinMode(pinF, INPUT);
   attachInterrupt(pinF, interrupt_tube_F, FALLING);
+  
+  // Set up the SD card
+  pinMode(SDPin, OUTPUT);
+  if(!sd.begin(SDPin, SPI_HALF_SPEED)){
+    Serial.println("SD Card initialization failed!");
+    while(true){}    // Should attempt to fix/format the SD card.
+  } else {
+    if(!sd.mkdir("/lbcc/rocksatc/")){
+      Serial.println("create folder \"/lbcc/rocksatc/\" failed.");
+    } else {
+      Serial.println("SD Card ready.");
+    }
+  }
 }
 
 void loop(){
   // Debugging only, fills the active buffer with sample data
-  /*
   while(true){
-    //process_interrupt(micros(), random(6));
+    process_interrupt(micros(), random(6));
   }
-  */
 }
 
 void write_to_sd(){
@@ -60,10 +78,26 @@ void write_to_sd(){
   */
   active_buffer = !active_buffer; // Switch between buffers
   
+  if(dataFile.open("/lbcc/rocksatc/rscdata.txt", O_RDWR | O_CREAT | O_AT_END)){
+    Serial.print("Writing buffer ");
+    Serial.print(!active_buffer);
+    Serial.print(" to the SD card...");
+    dataFile.println("New flush beginning here:");
+    for(int i=0; i<buffer_max; i++){
+      dataFile.print(buffer[!active_buffer][i].time);
+      dataFile.print("\t");
+      dataFile.println(buffer[!active_buffer][i].tubes);
+    }
+    Serial.println(" Write complete!");
+    dataFile.close();
+  } else {
+    Serial.println("Error opening file...");
+  }
   /*
   All of this code is for debugging only, and will be replaced by a
   write to the SD card.
   */
+  /*
   Serial.print("\nWriting buffer ");
   // !active_buffer refers to the inactive buffer
   Serial.print(!active_buffer); 
@@ -74,6 +108,7 @@ void write_to_sd(){
     Serial.print("\t");
     Serial.println(buffer[!active_buffer][i].tubes);
   }
+  */
 }
 
 /*

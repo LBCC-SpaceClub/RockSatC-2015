8ba6ceb7890f0d5470ad2de16e340950f3849047
lo9key <lo9key@gmail.com>
2015-07-04 08:01:55 -0700

    Created logs and graphs folders, adjusted testParse.py to compensate.  parselogs.py is now broken, but I'll come back to it.

--- testParse.py
+++ testParse.py
@@ -1,47 +1,57 @@
 import os, sys
 import matplotlib.pyplot as plt
 
-prefix = "LBCC_"
-logs = [f for f in os.listdir(os.curdir) if f.startswith(prefix)]
-radar = "NRW-5600 Terrier Improved Orion(RockOn-41.113 Koehler) R3 POSDAT 06252015.txt"
+flight_log = "logs/LBCC_00_Final.TXT"
+radar_log = "logs/RSC2015_RadarData/NRW-5600 Terrier Improved" \
+            " Orion(RockOn-41.113 Koehler) R3 POSDAT 06252015.txt"
 
 coTimes = []
 coAltitudes = []
 radTimes = []
 radAltitudes = []
 
+# Parse our flight data
+print "Opening flight log: " + flight_log
+with open(flight_log, "r") as f:
+    for hit in f:
+        '''
+        Sample data: 234440923\t111010
+        Data is recorded as microseconds, tube status
+        There are 6 tubes, each 0 represents a hit on that tube
+        More than one zero represents a coincidence between tubes
+        Catch: Leading zeros are omitted
+        '''
+        time, tubes = hit.split()
+        if tubes.rjust(6,'0').count('0')>1:
+            coTimes.append(round(int(time)/1000000.0,1))
 
-altitudes = []
-combo = []
-
-for log in logs:
-    print "Opening log: " + log
-    with open(log, "r") as data:
-        for hit in data:
-            time, tubes = hit.split()
-            if tubes.rjust(6,'0').count('0')>1:
-                coTimes.append(round(int(time)/1000000.0,1))
-
-with open(radar, "r") as f:
-    print "Opening radar log: " + radar
+# Parse the radar data
+print "Opening radar log: " + radar_log
+with open(radar_log, "r") as f:
     f.next()
     i = 0 # Index of coincidence
     for line in f:
+        '''
+        The radar log has a lot of data in it, but the important
+        sections are time and altitude. (maybe velocity?)
+        Time is the second col ([1]), and altitude is tenth ([9])
+        '''
         line = line.split()
+        # Had trouble going from '9.5' to 9 without using both?
         alt = int(float(line[9]))
-        time = round(float(line[1]),1)
+        time = round(float(line[1]), 1)
 
+        # Each radar station recorded a different flight length,
+        # but all we care about is the first 10 minutes.
         if time >= 600:
             break
 
         if i<len(coTimes) and time >= coTimes[i]:
             coAltitudes.append(alt)
-            # combo.append([coTimes[i], alt])
             i += 1
 
         radTimes.append(time)
         radAltitudes.append(alt)
-        # altitudes.append([time, alt])
 
 print "Graphing.."
 plt.scatter(coTimes, coAltitudes, label='Coincidences', color='r')

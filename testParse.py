1c88419465512acd37a22e25a9f68f3027208ae9
lo9key <lo9key@gmail.com>
2015-07-12 21:19:26 -0700

    Preparing to make a new branch for reworking the approach.

--- testParse.py
+++ testParse.py
@@ -7,8 +7,11 @@ radar_log = "logs/RSC2015_RadarData/NRW-5600 Terrier Improved" \
 
 coTimes = []
 coAltitudes = []
+hitTimes = []
+hitAltitudes = []
 radTimes = []
 radAltitudes = []
+eachTubeCount = [[] for x in range(6)]
 
 # Parse our flight data
 print "Opening flight log: " + flight_log
@@ -22,14 +25,27 @@ with open(flight_log, "r") as f:
         Catch: Leading zeros are omitted
         '''
         time, tubes = hit.split()
-        if tubes.rjust(6,'0').count('0')>1:
-            coTimes.append(round(int(time)/1000000.0,1))
+        tubes = tubes.rjust(6, '0')
+        time = round(int(time)/1000000.0,1)
+        if time >= 600:
+            break
+
+        if tubes.count('0')>1:
+            coTimes.append(time)
+        else:
+            hitTimes.append(time)
+
+        # Increment individual tube counts
+        for i in range(6):
+                if(tubes[i] == '0'):
+                    eachTubeCount[i].append(time)
 
 # Parse the radar data
 print "Opening radar log: " + radar_log
 with open(radar_log, "r") as f:
     f.next()
     i = 0 # Index of coincidence
+    j = 0 # Index of hit
     for line in f:
         '''
         The radar log has a lot of data in it, but the important
@@ -45,20 +61,35 @@ with open(radar_log, "r") as f:
         # but all we care about is the first 10 minutes.
         if time >= 600:
             break
-
         if i<len(coTimes) and time >= coTimes[i]:
             coAltitudes.append(alt)
             i += 1
+        elif j<len(hitTimes) and time >= hitTimes[j]:
+            hitAltitudes.append(alt)
+            j += 1
 
         radTimes.append(time)
         radAltitudes.append(alt)
 
 print "Graphing.."
+# subplot(212) designates room for 2 plots, vertical, with plt=1
+# plt.subplot(211)
+plt.plot(radTimes, radAltitudes, label='Radar Altitudes', color='k')
+plt.scatter(hitTimes, hitAltitudes, label='Hits', s=.5, color='b')
 plt.scatter(coTimes, coAltitudes, label='Coincidences', color='r')
-plt.plot(radTimes, radAltitudes, label='Radar Altitudes')
 plt.xlabel('Time (s)')
 plt.ylabel('Altitude (m)')
 plt.title('LBCC RocksatC Results')
 plt.axis([0, 600, 0, 120000])
 plt.legend()
+
+# plt.subplot(212) #plt=2
+# hitRoundedAlts = []
+# roundTo = 5000
+# for hit in hitAltitudes:
+#     hitRoundedAlts.append(round(hit/float(roundTo))*hit)
+# plt.plot(radTimes, radAltitudes, label='Radar Altitudes', color='k')
+# plt.scatter(hitTimes, hitRoundedAlts, label='Hits', s=.5, color='b')
+# plt.axis([0, 600, 0, 120000])
+# plt.legend()
 plt.show()
